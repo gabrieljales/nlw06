b9960b55f4d79c5dedf0f7836569ecfe2ec7c263
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-03-07 18:32:36 -0300

    Typeorm configuration

---
+++ ormconfig.ts
@@ -0,0 +1,14 @@
+module.exports = {
+  "type": "postgres",
+  "host": process.env.DATABASE_HOST,
+  "port": process.env.DATABASE_PORT,
+  "username": process.env.DATABASE_USER,
+  "password": process.env.DATABASE_PASSWORD,
+  "database": process.env.DATABASE_NAME,
+  "migrations": ["src/database/migrations/*.ts"],
+  "entities": ["src/entities/*.ts"],
+  "cli": {
+    "migrationsDir": "src/database/migrations",
+    "entitiesDir": "src/entities"
+  }
+}
