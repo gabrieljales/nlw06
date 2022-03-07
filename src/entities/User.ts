069f38a7850f4faf060c57bd29a9801b2c05e721
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-03-07 18:32:59 -0300

    User entitie defined

---
+++ src/entities/User.ts
@@ -0,0 +1,31 @@
+import { Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm";
+import { v4 as uuid } from 'uuid';
+
+@Entity('users')
+class User {
+  @PrimaryColumn()
+  readonly id: string; // Somente a própria entidade pode setar/alterar esse valor
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  admin: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid(); // Sempre que um usuário for criado, seta um uuid
+    }
+  }
+}
+
+export { User };
