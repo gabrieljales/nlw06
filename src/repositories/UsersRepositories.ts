513902532de191b45b2473b180b00385e05bf2dc
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-03-07 18:33:17 -0300

    User repository created

---
+++ src/repositories/UsersRepositories.ts
@@ -0,0 +1,9 @@
+import { EntityRepository, Repository } from 'typeorm';
+import { User } from '../entities/User';
+
+@EntityRepository(User)
+class UsersRepositories extends Repository<User> { }
+
+export { UsersRepositories };
+
+// Repositórios é a camada que faz a relação da entidade com a base de dados, são os responsáveis por terem os métodos que vão manipular o banco de dados (busca, inserção e etc)
