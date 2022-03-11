7eb768f9dc2f7d0e83a279c9848769a4d96fc729
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-03-11 18:23:21 -0300

    Password added to user

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,14 +1,16 @@
 import { getCustomRepository } from 'typeorm';
 import { UsersRepositories } from "../repositories/UsersRepositories";
+import { hash } from 'bcryptjs';
 
 interface IUserRequest {
   name: string;
   email: string;
   admin?: boolean;
+  password: string;
 };
 
 class CreateUserService {
-  async execute({ name, email, admin }: IUserRequest) {
+  async execute({ name, email, admin, password }: IUserRequest) {
     const usersRepositories = getCustomRepository(UsersRepositories); // getCustomRepository é o responsável por instanciar o UsersRepositories (não poderia ser new UsersRepositories)
 
     if (!email) {
@@ -23,10 +25,13 @@ class CreateUserService {
       throw new Error('User already exists');
     }
 
+    const passwordHash = await hash(password, 8);
+
     const user = usersRepositories.create({
       name,
       email,
-      admin
+      admin,
+      password: passwordHash,
     });
 
     await usersRepositories.save(user);
