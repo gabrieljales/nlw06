dd9932c97bfc36916543b499819fd272912b793f
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-03-12 19:15:28 -0300

    Refactoring: ensuring that SECRET_HASH will not be undefined (! operator)

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -31,21 +31,16 @@ class AuthenticateUserService {
 
     // Dica: Se ficar na dúvida entre usar ou não o await, veja o retorno
     // (o sign retorna uma string, enquanto o compare() acima retorna uma Promise<boolean> e por isso usa-se o await)
-    const secretHash = process.env.SECRET_HASH;
 
-    // Como secret hash também pode ser undefined, o TS não deixa utilizar ela no sign sem antes tratar
-    if (secretHash) {
-      const token = sign({
-        email: user.email
-      }, secretHash, {
-        subject: user.id,
-        expiresIn: "1d"
-      });
+    // Como secret hash também pode ser undefined, o TS não deixa utilizar ela no sign sem antes tratar. Porém, se adicionarmos uma exclamação depois do valor, asseguramos ao transpilador typescript que o valor não será indefinido
+    const token = sign({
+      email: user.email
+    }, process.env.SECRET_HASH!, {
+      subject: user.id,
+      expiresIn: "1d"
+    });
 
-      return token;
-    } else {
-      throw new Error('SECRET_HASH is not defined in .env!!')
-    }
+    return token;
   }
 }
 
