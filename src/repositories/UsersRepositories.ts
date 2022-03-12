023e162de20867b1eb93c2b811edc1f8336c323c
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-03-12 19:12:59 -0300

    Update repository info comment

--- src/repositories/UsersRepositories.ts
+++ src/repositories/UsersRepositories.ts
@@ -6,4 +6,4 @@ class UsersRepositories extends Repository<User> { }
 
 export { UsersRepositories };
 
-// Repositórios é a camada que faz a relação da entidade com a base de dados, são os responsáveis por terem os métodos que vão manipular o banco de dados (busca, inserção e etc)
+// Repositórios é a camada que faz a relação da entidade com a base de dados, são os responsáveis por terem os métodos que vão manipular o banco de dados (busca, inserção e etc). Sempre que um serviço ou middleware precisar conferir alguma informação no banco, devemos usar os repositórios
