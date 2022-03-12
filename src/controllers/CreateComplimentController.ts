45b748c775659dec19625aadd35658b433d2bf7f
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-03-12 19:20:23 -0300

    Using the logged in user as user_sender instead of receiving in request.body

--- src/controllers/CreateComplimentController.ts
+++ src/controllers/CreateComplimentController.ts
@@ -3,14 +3,15 @@ import { CreateComplimentService } from '../services/CreateComplimentService';
 
 class CreateComplimentController {
   async handle(request: Request, response: Response) {
-    const { tag_id, user_receiver, user_sender, message } = request.body;
+    const { tag_id, user_receiver, message } = request.body;
+    const { user_id } = request; // Usando id do usuário autenticado (assim ele é obrigado a se autenticar e n poderá manipular isso)
 
     const createComplimentService = new CreateComplimentService();
 
     const compliment = await createComplimentService.execute({
       tag_id,
       user_receiver,
-      user_sender,
+      user_sender: user_id, // Disponível por causa do middleware ensureAuthenticated (linha 24)
       message,
     });
 
